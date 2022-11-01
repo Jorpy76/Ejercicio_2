c56a95a63c8ec657e77d8664bf1e1c88be245bc7
Jorge Ríos <jorpy76@gmail.com>
2022-11-01 20:35:47 -0300

    ejercicio N°2

---
+++ ejercicio_2.js
@@ -0,0 +1,12 @@
+const datosPersonales = ["Jorge", 45, false, new Date("December 24, 1976") ]
+console.log(datosPersonales)
+
+const libroFavorito = {
+    titulo: "Winter of the world",
+    autor: "Ken Follet",
+    fecha: "2012",
+    url: "www.ken-follet.com"
+}
+
+console.log(typeof (datosPersonales[3]))
+console.log(libroFavorito)
